23013be5109ea47da5a1878545626d25f7be4039
Abhilash <abhilashanil40404@gmail.com>
2023-01-23 22:05:00 +0530

    idk

---
+++ js/sandbox.js
@@ -0,0 +1,9 @@
+
+
+// document.querySelector('.nav-link').scrollIntoView({behavior:'smooth',block:'end',inline:'end'})
+
+// .scrollIntoView({
+//     behavior: "smooth",
+//     block: "start",
+//     inline: "nearest"
+//   });
